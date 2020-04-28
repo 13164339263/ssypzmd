93eb7a11950c8fbfa19d1e576fcbd065a57df59f
13164339263 <13164339263.163.com>
2020-04-28 19:45:01 +0800

    'f'

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/ssypzmd": {
-        target: "http://127.0.0.1:8282/",
+        target: "http://127.0.0.1:8282/IDEASSM",
         changeOrigin: true,
         ws: true,
         pathRewrite: {
